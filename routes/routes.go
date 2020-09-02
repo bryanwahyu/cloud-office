a85724b45866851a02f713074c50878ff7e9b62e
Bryan <bryanwahyukp95@gmail.com>
2020-09-02 17:53:10 +0700

    golang-echo

---
+++ routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+import (
+	"net/http"
+	
+	"github.com/labstack/echo"
+)
+func Init() *echo.Echo {
+	e :=echo.New()
+
+	e.GET("/",func (c echo.Context) error {
+		return c.String(http.StatusOK,"coba golang")
+	})
+	return e
+}
